382e899a594e0ac897df494edcb7fd2a166ea9ec
Daniel Cabral <79099414+danieldemac@users.noreply.github.com>
2023-10-11 17:20:55 -0300

    Add files via upload

---
+++ app/Http/Controllers/FornecedorController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FornecedorController extends Controller
+{
+    public function index(){
+        $fornecedores = ['Fornecedor 1'];
+        return view('app\fornecedor\index', compact('fornecedores'));
+    }
+}
