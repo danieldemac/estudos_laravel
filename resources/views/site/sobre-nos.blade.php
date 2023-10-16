673906d3003d38deb6f2f867c3c1d50dc1c28ad3
Daniel Cabral <79099414+danieldemac@users.noreply.github.com>
2023-10-16 16:53:40 -0300

    Add files via upload

---
+++ resources/views/site/sobre-nos.blade.php
@@ -0,0 +1,34 @@
+     @extends('site.layouts.basico')
+     @section('titulo', 'Sobre Nós')
+     @section('conteudo')
+        
+        <div class="conteudo-pagina">
+            <div class="titulo-pagina">
+                <h1>Olá, eu sou o Super Gestão</h1>
+            </div>
+
+            <div class="informacao-pagina">
+                <p>O Super Gestão é o sistema online de controle administrativo que pode transformar e potencializar os negócios da sua empresa.</p>
+                <p>Desenvolvido com a mais alta tecnologia para você cuidar do que é mais importante, seus negócios!</p>
+            </div>  
+        </div>
+
+        <div class="rodape">
+            <div class="redes-sociais">
+                <h2>Redes sociais</h2>
+                <img src="img/facebook.png">
+                <img src="img/linkedin.png">
+                <img src="img/youtube.png">
+            </div>
+            <div class="area-contato">
+                <h2>Contato</h2>
+                <span>(11) 3333-4444</span>
+                <br>
+                <span>supergestao@dominio.com.br</span>
+            </div>
+            <div class="localizacao">
+                <h2>Localização</h2>
+                <img src="img/mapa.png">
+            </div>
+        </div>
+    @endsection
