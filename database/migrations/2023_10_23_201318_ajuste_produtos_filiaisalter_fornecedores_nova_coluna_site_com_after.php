bbdfda808b47a6325c3bbf8f90ed1982f96e0b21
Daniel Cabral <daniel.cabral@datametrica.com.br>
2023-10-31 14:52:26 -0300

    Primeiro Commit 2

--- database/migrations/2023_10_23_201318_ajuste_produtos_filiaisalter_fornecedores_nova_coluna_site_com_after.php
+++ database/migrations/2023_10_23_201318_ajuste_produtos_filiaisalter_fornecedores_nova_coluna_site_com_after.php
@@ -26,6 +26,7 @@ return new class extends Migration
         Schema::table('fornecedores', function(Blueprint $table){
             $table->dropColumn('site');
 
+            
         });
     }
 };
