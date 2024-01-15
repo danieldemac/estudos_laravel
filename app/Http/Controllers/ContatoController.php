613ad81114b74f949a702939431759294ae8e462
danieldemac <12dancabral@gmail.com>
2024-01-15 15:25:54 -0300

    Teste2

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,11 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato(){
-        var_dump($_POST);
-        return view('site.contato'); 
+    public function contato(Request $request){
+        // echo '<pre>';
+        // print_r($request->all());
+        // echo '</pre>';
+        // echo $request->input('nome');
+        // echo '<br>';
+        // echo $request->input('email');
+
+        // $contato = new SiteContato();
+        // $contato->nome = $request->input('nome');
+        // $contato->telefone = $request->input('telefone');
+        // $contato->email = $request->input('email');
+        // $contato->motivo_contato = $request->input('motivo_contato');
+        // $contato->mensagem = $request->input('mensagem');
+        // $contato->save();
+
+        $contato = new SiteContato();
+        $contato->create($request->all());
+        // $contato->save();
+
+
+        // print_r($contato->getAttributes());
+        
+        return view('site.contato', ['título' => 'Contato (teste)']); 
     }
 }
